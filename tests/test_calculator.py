50848150943ff0dacbcdc21ad951f955d13f2d81
Saheed Alpha Mansaray <mansaraysaheedalpha@gmail.com>
2026-08-23 22:28:55 +0000

    fixed formatting

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -2,7 +2,7 @@ from app.calculator import add, subtract
 
 
 def test_add():
-  assert add(4, 5) == 9
+    assert add(4, 5) == 9
 
 def test_subtract():
-  assert subtract(8, 2) == 6
+    assert subtract(8, 2) == 6
